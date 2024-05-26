eb9f0234081153b804492440c11ff2a8c5bc7b12
Paul_Wang(王浩仁) <Paul_Wang@pxmart.com.tw>
2024-05-27 02:45:31 +0800

    資料庫連接好，並建立Products

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Globalization;
+using WebApplication1.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+
 // Add services to the container.
+
+builder.Services.AddDbContext<ProductContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("StrConnection")));
+        
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
